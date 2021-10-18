c045d54bfdd27dfc3db965bd27b7b1a394196071
christinaverenahue <christina.huebers@student.fh-kiel.de>
2021-10-18 14:04:55 +0200

    Add osm data & demo twitter use

---
+++ code/OSM/overpass_api.py
@@ -0,0 +1,44 @@
+import json
+import requests
+
+import geopandas as gpd
+
+# Retrieve HH district boundaries from Overpass API
+overpass_url = "http://overpass-api.de/api/interpreter"
+
+overpass_query = """
+[out:json];
+area["ISO3166-2"="DE-HH"];
+rel["boundary"="administrative"]["admin_level"=10](area);
+convert item ::=::,::geom=geom(),_osm_type=type();
+out geom;
+"""
+response = requests.get(overpass_url, 
+                        params={'data': overpass_query})
+
+"""
+---Interlude---
+>>convert item ::=::,::geom=geom(),_osm_type=type();<< is for 'geojson' 
+--> without the output would be plain json 
+
+But you think you're done? No way, José :') This is still no valid geojson.
+Valid geojson has to look like this: https://datatracker.ietf.org/doc/html/rfc7946#section-1
+
+"""
+# Converting to valid geojson format
+response_data = response.json()['elements']
+
+out = {"type": "FeatureCollection",
+  "features": []}
+
+for feature in response_data:
+    feature["type"] = 'Feature'
+    out['features'].append(feature)
+
+# Save geojson file
+with open('Hamburg.geojson', 'w') as geojson:
+    json.dump(out, geojson)
+
+# Read in geojson file and plot
+plz_shape_df = gpd.read_file('Hamburg.geojson')
+plz_shape_df.plot() # ugly indeed but Hamburg is recognizable 
---
+++ code/Twitter API/Demo Tweets/demo_tweets.py
@@ -0,0 +1,34 @@
+import os
+
+import geopandas as gpd
+import pymongo
+import tweepy
+from dotenv import load_dotenv
+
+
+# Set up twitter API
+load_dotenv('../../.env')
+auth = tweepy.OAuthHandler(os.getenv("CONSUMER_KEY"),
+                           os.getenv("CONSUMER_SECRET"))  # app
+auth.set_access_token(os.getenv("ACCESS_TOKEN"),
+                      os.getenv("ACCESS_TOKEN_SECRET"))  # user
+api = tweepy.API(auth)
+
+# Set up MongoDB
+client = pymongo.MongoClient('localhost:27017')
+db = client["webgefluester"]
+
+# Retrieve district names
+plz_shape_df = gpd.read_file('../../OSM/Hamburg.geojson')
+names = [district['name'] for district in plz_shape_df.tags]
+
+for district in names:
+    tweets = api.search_tweets(q=district, count=3) # beware of rate limit (450 per 15 minutes)
+    collection = db[district] # new mongo collection
+
+    for tweet in tweets:
+        try:
+            collection.insert_one(tweet._json)
+        except pymongo.errors.DuplicateKeyError:
+            continue
+
