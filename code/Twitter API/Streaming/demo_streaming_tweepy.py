a4ce33c91326e2725be29187bce87ef1f5a76ffa
Julian Berger <julian.berger@haw-hamburg.de>
2021-11-18 11:52:28 +0100

    added try/except to prevent stream from breaking

--- code/Twitter API/Streaming/demo_streaming_tweepy.py
+++ code/Twitter API/Streaming/demo_streaming_tweepy.py
@@ -6,29 +6,6 @@ import tweepy
 from dotenv import load_dotenv
 import geopandas as gpd
 
-# Retrieve district names
-#plz_shape_df = gpd.read_file('../../OSM/Hamburg.geojson')
-#names = [district['name'] for district in plz_shape_df.tags]
-#places = gpd.read_file('../../Mapping/HH_WFS_Gruenflaechen.gml', driver ='GML')
-#places_names = [place.lstrip() for name in places.name for place in name.split(',') if len(place) < 60]
-#to_track = names + places_names
-
-# excluded Altstadt & Neustadt for demonstrative purposes 
-#osm_Altona_streets = gpd.read_file('../../OSM/Altona_streets.geojson')
-#streets = gpd.read_file('../../Mapping/HH_WFS_Strassen_und_Wegenetz_gesamt', driver='GML')
-#altona_streets = streets[streets.gemeindeschluessel.isin(list(range(201,207)))].set_crs('ETRS89', allow_override= True)
-#double_streets = {row['properties']['strassenname'] for row in altona_streets.iterfeatures() if len(streets[(streets.strassenname == row['properties']['strassenname']) & ~(streets.gemeindeschluessel.isin(list(range(201,207))))]) > 1}
-#parks = gpd.read_file('../../Mapping/HH_WFS_Gruenplan.gml', driver='GML')
-#parks[parks.verwaltungsvermoegen == "Flächen des Bezirks - Stadtgrün"].sort_values("flaeche_ha", ascending = False).head()
-#parks.drop("geometry", axis = 1)['nutz_code'].value_counts()
-#parks.to_csv("gruenplan.csv")
-
-
-# Twitter restricts tracked phrases to 60 bytes
-#altona_street_names = {name for name in altona_streets.strassenname if len(name) < 60}
-#altona_parks = set(parks[(parks.ortsteil.isin(list(range(201,207)))) & (parks.benennung is not None)].benennung)
-
-
 #to_track = altona_street_names.union(altona_parks).union(phrases)
 # Set up twitter API
 load_dotenv('../../.env') # .env file in 'code' dir
@@ -51,12 +28,23 @@ class MyStream(tweepy.Stream):
         print(tweet["created_at"]+":", tweet["text"], "\n", "-----")
         #self.tweet_raw = data
 
-#setup stream authentication
-stream = MyStream(os.getenv("CONSUMER_KEY"),
+
+
+# function to start the stream with exeptions 
+
+def start_stream():
+    while True:
+        try:
+            #setup stream authentication
+            stream = MyStream(os.getenv("CONSUMER_KEY"),
                         os.getenv("CONSUMER_SECRET"),
                         os.getenv("ACCESS_TOKEN"),
                         os.getenv("ACCESS_TOKEN_SECRET"))
+            stream.filter(track=list(to_track), languages = ['de'])
+        except KeyboardInterrupt: 
+            break
+        except:
+            continue
 
-# setup the stream, break it to look at the tweet_raw
-stream.filter(track=list(to_track), languages = ['de'])
+start_stream()
 #locations=[10.6444, 53.3960, 9.5663, 53.7516]
